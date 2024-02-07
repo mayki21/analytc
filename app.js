da367aee8cc3b743dfc14c4f1f402d93bcbf79dc
mayki21 <mayank.khobragade9891@gmail.com>
2024-02-07 10:49:49 +0530

    changes

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
-// const express = require("express");
-// const connection = require("./connection/db");
-// require('dotenv').config();
-// const userRoutes = require("./routes/user");
 
-// const app = express();
-// app.use(express.json());
-
-// // Register user routes
-// app.use("/api/user", userRoutes);
-
-// app.listen(process.env.PORT, async () => {
-//   try {
-//     await connection;
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//   }
-//   console.log(`Server is running at ${process.env.PORT}`);
-// });
 const express = require("express");
 const connection = require("./connection/db");
 require('dotenv').config();
