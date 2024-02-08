40d2c01d1875252590b8fb8f768de86cbec33ebe
mayki21 <mayank.khobragade9891@gmail.com>
2024-02-08 16:25:55 +0530

    login api update

--- routes/user.js
+++ routes/user.js
@@ -59,6 +59,48 @@ router.post("/register", async (req, res) => {
 //   }
 // });
 
+// router.post("/login", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Check if the user exists
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(401).json({ message: "Invalid email or password" });
+//     }
+
+//     // Check if the passwords match
+//     if (user.password !== password) {
+//       return res.status(401).json({ message: "Invalid email or password" });
+//     }
+
+//     // If everything is okay, user is authenticated
+//     // Generate JWT token
+//     const token = jwt.sign(
+//       { userId: user._id, userType: user.userType },
+//       process.env.JWT_SECRET,
+//       { expiresIn: "2h" }
+//     );
+    
+//     // Return token to the client along with userType and userId
+//     let response = { message: "Login successful", token, userType: user.userType, userId: user._id };
+
+//     // If userType is agent, include agentId in the response
+//     if (user.userType === 'agent') {
+//       response.agentId = user._id; // Assuming agentId is the same as userId
+//     }
+//     // If userType is admin, include adminId in the response
+//     else if (user.userType === 'admin') {
+//       response.adminId = user._id; // Assuming adminId is the same as userId
+//     }
+
+//     res.status(200).json(response);
+//   } catch (error) {
+//     console.error("Error logging in:", error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// });
+
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -82,8 +124,8 @@ router.post("/login", async (req, res) => {
       { expiresIn: "2h" }
     );
     
-    // Return token to the client along with userType and userId
-    let response = { message: "Login successful", token, userType: user.userType, userId: user._id };
+    // Return token to the client along with userType
+    let response = { message: "Login successful", token, userType: user.userType };
 
     // If userType is agent, include agentId in the response
     if (user.userType === 'agent') {
@@ -100,6 +142,7 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
+
 
 
 
